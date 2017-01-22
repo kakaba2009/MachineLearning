45db7ff50192899e71d1c4a6a81e1634d84a144c
kakaba2009 <kakaba2009@gmail.com>
2017-01-22 11:59:36 +0900

    Added Read CSV

--- python/src/plot/FXData.py
+++ python/src/plot/FXData.py
@@ -2,9 +2,10 @@ import src.lib.mfile as mfile
 import src.lib.mcalc as mcalc
 import src.lib.mplot as mplot
 
-df = mfile.loadSymbolList("../db/forex.db")
-df = df["Symbol"]
-df = mfile.loadOneSymbol("JPY=X", "../db/forex.db")
+#df = mfile.loadSymbolList("../db/forex.db")
+#df = df["Symbol"]
+#df = mfile.loadOneSymbol("JPY=X", "../db/forex.db")
+df = mfile.loadCSV("JPY=X")
 df = df[-1000:]
 #df = m_sample_m(df)
 df = df[['High']]
--- r/learn/takens.R
+++ r/learn/takens.R
@@ -1,4 +1,5 @@
 library(nonlinearTseries)
+library(xts)
 
 source('./lib/mcalc.R')
 source('./lib/mtool.R')
@@ -6,24 +7,25 @@ source('./lib/mfractal.R')
 
 #saveCSV('JPY=X')
 
-x <- loadSymbol('JPY=X')
-x <- loadTimeSeries('JPY=X', 'High')
-#x <- as.ts(forex['x'])
-#x <- x[1:1000,]
+df <- loadSymbol('JPY=X')
+df <- df$High
+ts <- as.ts(df)
+ts <- tail(ts, n=1000)
+#to.weekly(ts)
 
-#lag <- calcTimeLag(x)
+#lag <- calcTimeLag(ts)
 
-#calcLyapunov(x, 30)
-#d <- BoxCountDim(x)
-#cd <- CorrDim(x, d1=5, d2=9, r1=0.05, r2=1, lag=1)
+#calcLyapunov(ts, 30)
+#d <- BoxCountDim(ts)
+#cd <- CorrDim(ts, d1=5, d2=9, r1=0.05, r2=1, lag=1)
 #e  <- SampleEntropy(cd, d1=6, d2=9, r1=0.1, r2=1.0)
-d <- calcBestDimension(x, 1, 0.90)
-t <- calcTakens(x, d, 1)
-r <- calcInfoDimension(x, 7, 1)
+d <- calcBestDimension(ts, 1, 0.90)
+t <- calcTakens(ts, d, 1)
+r <- calcInfoDimension(ts, 7, 1)
 
-calcRecurrPlot(t, x, d, 1, r*2.0)
-calcRQA(t, x, d, r*2.0)
-#AdditiveNonlinearAutoregressive(x, 7)
+calcRecurrPlot(t, ts, d, 1, r*2.0)
+calcRQA(t, ts, d, r*2.0)
+#AdditiveNonlinearAutoregressive(ts, 7)
 #SpaceTimePlot(t, d, 1000)
-#n = NeuralNetworkNonlinearAutoregressiveModel(x, d, 1, 1, 8)
-#f <- FindDominantFrequency(log(x))
+#n = NeuralNetworkNonlinearAutoregressiveModel(ts, d, 1, 1, 8)
+#f <- FindDominantFrequency(log(ts))
