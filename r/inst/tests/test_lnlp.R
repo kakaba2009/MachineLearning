b5c6c4628899bf99cb7e8c2c6f60a0e3fa29b61d
kakaba2009 <kakaba2009@gmail.com>
2017-02-05 17:35:02 +0900

    Added Small Change

--- r/inst/tests/test_lnlp.R
+++ r/inst/tests/test_lnlp.R
@@ -1,21 +1,17 @@
 library(rEDM)
-library(data.table)
 
-source("./mylib/mtool.R")
+data("two_species_model")
 
-#data("two_species_model")
-two_species_model <- loadTimeSeries('JPY=X', 'High')
-
-ts <- tail(two_species_model, n=1000)
+ts <- two_species_model$x[1:200]
 
 # univariate simplex projection using E = 1:10, and leave-one-out cross-validation
 x <- simplex(ts, stats_only = FALSE)
 
 # univariate simplex projection using E = 1:10, and first half to predict second half
-simplex(ts, lib = c(1, 900), pred = c(901, 1000))
+simplex(ts, lib = c(1, 100), pred = c(101, 200))
 
-# univariate s-map using E = 7
-s_map(ts, E = 7)
+# univariate s-map using E = 2
+s_map(ts, E = 2)
 
-# univariate s-map using E = 7, theta = 1, and full output with smap_coefficients
-y <- s_map(ts, E = 7, theta = 1, save_smap_coefficients = TRUE)
+# univariate s-map using E = 2, theta = 1, and full output with smap_coefficients
+y <- s_map(ts, E = 2, theta = 1, save_smap_coefficients = TRUE)
