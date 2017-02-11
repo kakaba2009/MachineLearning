db62bdb3cf15cc7e3b4fd9ebbc51957899bd0b7e
kakaba2009 <kakaba2009@gmail.com>
2017-02-11 16:48:32 +0900

    Added Small Change

--- r/learn/s_map.R
+++ r/learn/s_map.R
@@ -3,21 +3,27 @@ library(rEDM)
 source('./mylib/mcalc.R')
 source('./mylib/mtool.R')
 
+options(max.print=5.5E5)
+
 df <- loadSymbol('JPY=X')
-df <- df[ ,c('Close')]
+#df$Close.Chg <- df$Close - shift(df$Close, n=1)
+df <- df$Close
 df <- tail(df, 1000)
+nr <- NROW(df)
 
 lib  <- c(1,   975)
-pred <- c(976, 1001)
+pred <- c(976, nr)
 
 BestE <- BestDimEDM(df, lib, pred)
 BestTheta <- BestThetaEDM(df, lib, pred, BestE)
 
-smap_output <- s_map(df, lib, pred, E=BestE, theta = c(BestTheta), stats_only=FALSE)
+smap_output <- s_map(df, lib, pred, E=BestE, theta = c(BestTheta), 
+                     stats_only=FALSE, save_smap_coefficients=TRUE)
 
 observed  <- smap_output[[1]]$model_output$obs
 predicted <- smap_output[[1]]$model_output$pred
-print(tail(predicted))
+print(tail(observed,  5))
+print(tail(predicted, 5))
 par(mar = c(4, 4, 1, 1), pty = "s")
 plot_range <- range(c(observed, predicted), na.rm = TRUE)
 plot(observed, predicted, xlim = plot_range, ylim = plot_range, xlab = "Observed", ylab = "Predicted")
--- r/learn/simplex.R
+++ r/learn/simplex.R
@@ -4,24 +4,31 @@ library(rEDM)
 source('./mylib/mcalc.R')
 source('./mylib/mtool.R')
 
+options(max.print=5.5E5)
+
 start = "1990-01-01"
 
 #df <- getQuandl("CURRFX/USDJPY", "daily", start, "raw")
 #df <- getQuandl("CHRIS/CME_GC1", "daily", start, "raw")
 df <- loadSymbol('JPY=X')
-df$Close.Chg <- df$Close - shift(df$Close, n=1)
-df <- df$Close.Chg
-df <- tail(df, 5000)
+#df$Close.Chg <- df$Close - shift(df$Close, n=1)
+df <- df$Close
+df <- tail(df, 1000)
+nr <- NROW(df)
 
-tar <- 4500
+tar <- 975
 lib  <- c(1,     tar)
-pred <- c(tar+1, 5000)
+pred <- c(tar+1, nr)
 
-BestE <- BestDimEDM(df, lib, pred, E=2:12, nn=10)
+BestE <- BestDimEDM(df, lib, pred, E=2:12)
 cat("BestE: ", BestE, "\n")
 
-simplex_output <- simplex(df, lib, pred, E=BestE, tp=1, num_neighbors=10, stats_only=FALSE)
+simplex_output <- simplex(df, lib, pred, E=BestE, tp=1, stats_only=FALSE)
 observed  <- simplex_output[[1]]$model_output$obs
 predicted <- simplex_output[[1]]$model_output$pred
 print(tail(observed,  5))
 print(tail(predicted, 5))
+par(mar = c(4, 4, 1, 1), pty = "s")
+plot_range <- range(c(observed, predicted), na.rm = TRUE)
+plot(observed, predicted, xlim = plot_range, ylim = plot_range, xlab = "Observed", ylab = "Predicted")
+abline(a = 0, b = 1, lty = 2, col = "blue")
