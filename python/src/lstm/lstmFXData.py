7ef5e326ea4a3e293e28519f7fde1095ad925780
kakaba2009 <kakaba2009@gmail.com>
2017-03-04 21:24:45 +0900

    Added Small Change

--- python/src/lstm/lstmFXData.py
+++ python/src/lstm/lstmFXData.py
@@ -1,62 +1,52 @@
-import numpy
+import numpy as np
 import src.mylib.mcalc as mcalc
 import src.mylib.mlstm as mlstm
+from sklearn.preprocessing import normalize
+from sklearn.model_selection import KFold
+
+loaded = False
+modelSaved = "../model/JPYRMSPropLinear12x6xD6.h5"
 
 batch_size = 1000
-epochs_num = 5
+epochs_num = 1
 
-seqn_size  = 6
+lag = 1
+dim = seqn_size = 6
 output_dim = 6
 
-numpy.random.seed(7) # fix random seed for reproducibility
+np.random.seed(6) # fix random seed for reproducibility
 
-LOG     = False
 dataset = mlstm.loadFXData('JPY=X', '../db/forex.db', 1000)
-dataset = dataset[['High']]
+dataset = dataset[['Close']]
 dataset = mcalc.m_pct(dataset, True)
-dataset = dataset.values
-dataset = dataset.astype('float32')    
-SCALE   = 1 #np.amax(dataset.flatten())
-print("SCALE=>", SCALE)
+SCALE   = np.amax(dataset.values.flatten())
 
-print(dataset[-seqn_size:])
+T = mcalc.vector_delay_embed(dataset, dim, lag)
 
-dataset = mlstm.normalize(dataset, SCALE, LOG)
+X, y = mcalc.split_x_y(T)
 
-trainX, trainY, testX, testY = mlstm.setupTrainTest(dataset, seqn_size, SCALE, LOG)
-# reshape input to be [samples, time steps, features]
-trainX = numpy.reshape(trainX, (trainX.shape[0], -1, trainX.shape[1]))
-testX1 = numpy.reshape(testX,  (testX.shape[0],  -1, testX.shape[1]))
-# create and fit the LSTM network
-modelSaved = "../model/JPYRMSPropLinear12x6xD6.h5"
-model = mlstm.loadModel(modelSaved, batch_size, trainX.shape, loss='mean_squared_error', 
-                  opt="RMSProp", stack=1, state=False, od=output_dim, act='linear', neurons=12)
+def mshape(X):
+    return np.reshape(X, (X.shape[0],  -1, X.shape[1])) 
 
-mlstm.trainModel(model, trainX, trainY, batch_size, epochs_num, modelSaved)
+kf = KFold(n_splits=2, shuffle=False, random_state=None)
+
+for train_index, test_index in kf.split(X):
+    trainX, testX = X[train_index], X[test_index]
+    trainY, testY = y[train_index], y[test_index]
+    
+    trainX = normalize(trainX, norm='l2')
+    trainY = normalize(trainY, norm='l2')
+    # reshape input to be [samples, time steps, features]
+    trainX = np.reshape(trainX, (trainX.shape[0], -1, trainX.shape[1]))
+    # create and fit the LSTM network
+    if(loaded == False):
+        model = mlstm.loadModel(modelSaved, batch_size, trainX.shape, loss='mean_squared_error', 
+                      opt="RMSProp", stack=1, state=False, od=output_dim, act='linear', neurons=12)
+        loaded = True
+
+    mlstm.trainModel(model, trainX, trainY, batch_size, epochs_num, modelSaved)
 
 # make predictions
-trainPredict = model.predict(trainX, batch_size=batch_size)
-#model.reset_states()
-testPredict  = model.predict(testX1, batch_size=batch_size)
-#model.reset_states()
-lastX,lastX1 = mlstm.lastInputSeq(dataset, seqn_size)
-lastPredict  = model.predict(lastX1,  batch_size=batch_size)
-#model.reset_states()
+predict = model.predict(mshape(X), batch_size=batch_size)
 
-mlstm.printScore(model, trainX, trainY)
-mlstm.printScore(model, testX1,  testY)
-#inverse scale
-dataset      = mlstm.inverse_transform(dataset, SCALE, LOG)
-#print(dataset[-seqn_size:])
-
-testX        = mlstm.inverse_transform(testX, SCALE, LOG)
-lastX        = mlstm.inverse_transform(lastX, SCALE, LOG)
-testPredict  = mlstm.inverse_transform(testPredict,  SCALE, LOG)
-lastPredict  = mlstm.inverse_transform(lastPredict,  SCALE, LOG)
-trainPredict = mlstm.inverse_transform(trainPredict, SCALE, LOG)
-mlstm.printX_Y(testX, testPredict)
-
-print("Tomorrow:", lastX, "=>", lastPredict)
-
-X, Y = mlstm.create_dataset(dataset, seqn_size)
-mlstm.plot_result_2F(X, trainPredict, testPredict, seqn_size)
+mlstm.plot_result_2F(y, predict, None, seqn_size)
--- python/src/mylib/mlstm.py
+++ python/src/mylib/mlstm.py
@@ -269,23 +269,20 @@ def plot_all_feature(X, c):
         plt.scatter(np.arange(F.shape[0]), F, color=c)
         
     
-def plot_result_2F(dataset, train, test, seqn_size):
+def plot_result_2F(dataset, predict, test=None, seqn_size=1):
     #input array shape is (, 2)
     style.use('ggplot')
     
-    # shift train predictions for plotting
-    trainPr       = np.empty_like(dataset)
-    trainPr[:, :] = np.nan
-    trainPr[seqn_size:len(train)+seqn_size, :] = train
-    # shift test predictions for plotting
-    testPre       = np.empty_like(dataset)
-    testPre[:, :] = np.nan
-    testPre[-1*len(test):, :] = test
-    
     # plot baseline and predictions
     plot_all_feature(dataset, "blue")
-    plot_all_feature(trainPr, "green")
-    plot_all_feature(testPre, "red")
+    plot_all_feature(predict, "green")
+    
+    if(test != None):
+        # shift test predictions for plotting
+        testPre       = np.empty_like(dataset)
+        testPre[:, :] = np.nan
+        testPre[-1*len(test):, :] = test
+        plot_all_feature(testPre, "red")
         
     mng = plt.get_current_fig_manager()
     mng.window.showMaximized()
@@ -310,3 +307,22 @@ def setupTrainTest(dataset, seqn_size, SCALE, LOG):
     printX_YScaler(testX, testY, SCALE, LOG)
     
     return trainX, trainY, testX, testY
+
+def plot_results(predicted_data, true_data):
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+    plt.plot(predicted_data, label='Prediction')
+    plt.legend()
+    plt.show()
+
+def plot_results_multiple(predicted_data, true_data, prediction_len):
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+    #Pad the list of predictions to shift it in the graph to it's correct start
+    for i, data in enumerate(predicted_data):
+        padding = [None for p in range(i * prediction_len)]
+        plt.plot(padding + data, label='Prediction')
+        plt.legend()
+    plt.show()
