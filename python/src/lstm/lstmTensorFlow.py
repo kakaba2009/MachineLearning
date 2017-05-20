bcf4d28105eb14febec9feace6f3b8ae4584142e
kakaba2009 <kakaba2009@gmail.com>
2017-05-20 20:22:46 +0900

    Add Tensor Flow

--- python/src/lstm/lstmTensorFlow.py
+++ python/src/lstm/lstmTensorFlow.py
@@ -17,7 +17,7 @@ warnings.filterwarnings("ignore")  # Hide messy Numpy warnings
 tf.set_random_seed(0)  # fix random seed
 
 # Hyper Parameters
-EPOCHSIZE = 50
+EPOCHSIZE = 5
 BATCHSIZE = 10
 TIME_STEP = 5       # rnn time step
 FEATURES = 1        # rnn input size
@@ -54,14 +54,9 @@ net_outs2D = tf.layers.dense(outs2D, FEATURES)
 outs = tf.reshape(net_outs2D, [-1, TIME_STEP, FEATURES])          # reshape back to 3D
 
 loss = tf.losses.mean_squared_error(labels=tf_y, predictions=outs)  # compute cost
-train_op = tf.train.AdamOptimizer(LR).minimize(loss)
+tf.summary.scalar("loss", loss)
 
-# Init Tensorboard stuff. This will save Tensorboard information into a different
-# folder at each run named 'log/<timestamp>/'. Two sets of data are saved so that
-# you can compare training and validation curves visually in Tensorboard.
-timestamp = str(math.trunc(time.time()))
-summary_writer = tf.summary.FileWriter("../log/" + timestamp + "-training")
-validation_writer = tf.summary.FileWriter("../log/" + timestamp + "-validation")
+train_op = tf.train.AdamOptimizer(LR).minimize(loss)
 
 # Init for saving models. They will be saved into a directory named 'checkpoints'.
 # Only the last checkpoint is kept.
@@ -69,40 +64,44 @@ if not os.path.exists("checkpoints"):
     os.mkdir("checkpoints")
 saver = tf.train.Saver(max_to_keep=1)
 
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())     # initialize var in graph
-#summary_writer.add_summary(sess)
+merged = tf.summary.merge_all()
 
-#plt.figure(1, figsize=(12, 5))
-#plt.ion()
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())     # initialize var in graph
 
-for epoch in range(EPOCHSIZE):
-    steps = 0
-    for x, y in tl.iterate.seq_minibatches(inputs=S, targets=T, batch_size=BATCHSIZE, seq_length=TIME_STEP, stride=1):
-        steps += 1
+    summary_writer = tf.summary.FileWriter("../log/", sess.graph)
 
-        x = mshape(x)  # shape (batch, time_step, input_size)
-        y = mshape(y)  # shape (batch, time_step, input_size)
-        print(steps)
-        #print("x=", x.flatten())  # shape (batch*time_step, input_size)
-        #print("y=", y.flatten())  # shape (batch*time_step, input_size)
+    #plt.figure(1, figsize=(12, 5))
+    #plt.ion()
 
-        if 'final_s_' not in globals():                # first state, no any hidden state
-            feed_dict = {tf_x: x, tf_y: y}
-        else:                                           # has hidden state, so pass it to rnn
-            feed_dict = {tf_x: x, tf_y: y, init_s: final_s_}
-        _, pred_, final_s_ = sess.run([train_op, outs, final_s], feed_dict)     # train
+    for epoch in range(EPOCHSIZE):
+        steps = 0
+        for x, y in tl.iterate.seq_minibatches(inputs=S, targets=T, batch_size=BATCHSIZE, seq_length=TIME_STEP, stride=1):
+            steps += 1
 
-        if steps % 2 == 0:
-            # Calculate batch loss
-            ls = sess.run(loss, feed_dict={tf_x: x, tf_y: y})
-            print("Iter " + str(steps) + ", Minibatch Loss= " + "{:.6f}".format(ls))
+            x = mshape(x)  # shape (batch, time_step, input_size)
+            y = mshape(y)  # shape (batch, time_step, input_size)
+            print(steps)
+            #print("x=", x.flatten())  # shape (batch*time_step, input_size)
+            #print("y=", y.flatten())  # shape (batch*time_step, input_size)
 
-        # plotting
-        #plt.plot(y.flatten(), 'r-')
-        #plt.plot(pred_.flatten(), 'b-')
-        #plt.draw()
-        #plt.pause(0.05)
+            if 'final_s_' not in globals():                # first state, no any hidden state
+                feed_dict = {tf_x: x, tf_y: y}
+            else:                                           # has hidden state, so pass it to rnn
+                feed_dict = {tf_x: x, tf_y: y, init_s: final_s_}
+            _, pred_, final_s_ = sess.run([train_op, outs, final_s], feed_dict)     # train
 
-#plt.ioff()
-#plt.show()
+            if steps % 2 == 0:
+                # Calculate batch loss
+                bloss, summary = sess.run([loss, merged], feed_dict={tf_x: x, tf_y: y})
+                print("Iter " + str(steps) + ", Minibatch Loss= " + "{:.6f}".format(bloss))
+                summary_writer.add_summary(summary, steps)
+
+            # plotting
+            #plt.plot(y.flatten(), 'r-')
+            #plt.plot(pred_.flatten(), 'b-')
+            #plt.draw()
+            #plt.pause(0.05)
+
+    #plt.ioff()
+    #plt.show()
