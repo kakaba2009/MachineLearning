158f9075d55359841af30ce76dac06586b910531
kakaba2009 <kakaba2009@gmail.com>
2017-05-22 21:59:09 +0900

    Add Tensor Flow

--- python/src/lstm/lstmTensorFlow.py
+++ python/src/lstm/lstmTensorFlow.py
@@ -20,9 +20,13 @@ tf.set_random_seed(0)  # fix random seed
 EPOCHSIZE = 25      # rnn epoch size
 BATCHSIZE = 10      # rnn batch size
 TIME_STEP = 5       # rnn time step
+CELLSIZE = 32       # rnn cell size
 FEATURES = 1        # rnn input size
-CELL_SIZE = 32      # rnn cell size
-LR = 0.001          # learning rate
+DROPOUTS = 0.5
+
+max_learning_rate = 0.001
+min_learning_rate = 0.00001
+decay_speed = 1000.0
 
 ds = mlstm.loadFXData('JPY=X', '../db/forex.db', 1000)
 ds = ds[['Close']].values
@@ -38,9 +42,12 @@ def mshape(X):
 # tensorflow placeholders
 tf_x = tf.placeholder(tf.float32, [None, TIME_STEP, FEATURES])        # shape(batch, steps, feature)
 tf_y = tf.placeholder(tf.float32, [None, TIME_STEP, FEATURES])        # input y
+# variable learning rate
+lr = tf.placeholder(tf.float32)
 
 # RNN
-rnn_cell = tf.contrib.rnn.BasicRNNCell(num_units=CELL_SIZE)
+rnn_cell = tf.contrib.rnn.BasicLSTMCell(num_units=CELLSIZE, forget_bias=1.0, state_is_tuple=True)
+rnn_cell = tf.contrib.rnn.DropoutWrapper(rnn_cell, input_keep_prob=DROPOUTS)
 init_s = rnn_cell.zero_state(batch_size=BATCHSIZE, dtype=tf.float32)    # very first hidden state
 outputs, final_s = tf.nn.dynamic_rnn(
     rnn_cell,                   # cell you have chosen
@@ -48,14 +55,14 @@ outputs, final_s = tf.nn.dynamic_rnn(
     initial_state=init_s,       # the initial hidden state
     time_major=False,          # False: (batch, time step, input); True: (time step, batch, input)
 )
-outs2D = tf.reshape(outputs, [-1, CELL_SIZE])                     # reshape 3D output to 2D for fully connected layer
+outs2D = tf.reshape(outputs, [-1, CELLSIZE])                     # reshape 3D output to 2D for fully connected layer
 net_outs2D = tf.layers.dense(outs2D, FEATURES)
-outs = tf.reshape(net_outs2D, [-1, TIME_STEP, FEATURES])          # reshape back to 3D
+outs = tf.reshape(net_outs2D, [-1, TIME_STEP, FEATURES])         # reshape back to 3D
 
 loss = tf.losses.mean_squared_error(labels=tf_y, predictions=outs)  # compute cost
 tf.summary.scalar("loss", loss)
 
-train_op = tf.train.AdamOptimizer(LR).minimize(loss)
+train_op = tf.train.AdamOptimizer(lr).minimize(loss)
 
 # Init for saving models. They will be saved into a directory named 'checkpoints'.
 # Only the last checkpoint is kept.
@@ -75,8 +82,8 @@ with tf.Session() as sess:
     #plt.figure(1, figsize=(12, 5))
     #plt.ion()
 
+    steps = 0
     for epoch in range(EPOCHSIZE):
-        steps = 0
         for x, y in tl.iterate.seq_minibatches(inputs=S, targets=T, batch_size=BATCHSIZE, seq_length=TIME_STEP, stride=1):
             steps += 1
 
@@ -86,15 +93,19 @@ with tf.Session() as sess:
             #print("x=", x.flatten())  # shape (batch*time_step, input_size)
             #print("y=", y.flatten())  # shape (batch*time_step, input_size)
 
+            # learning rate decay
+            learning_rate = min_learning_rate + (max_learning_rate - min_learning_rate) * math.exp(-steps / decay_speed)
+
             if 'final_s_' not in globals():                # first state, no any hidden state
-                feed_dict = {tf_x: x, tf_y: y}
+                feed_dict = {tf_x: x, tf_y: y, lr: learning_rate}
             else:                                           # has hidden state, so pass it to rnn
-                feed_dict = {tf_x: x, tf_y: y, init_s: final_s_}
+                feed_dict = {tf_x: x, tf_y: y, lr: learning_rate, init_s: final_s_}
+
             _, pred_, final_s_ = sess.run([train_op, outs, final_s], feed_dict)     # train
 
             if steps % 2 == 0:
                 # Calculate batch loss
-                bloss, summary = sess.run([loss, merged], feed_dict={tf_x: x, tf_y: y})
+                bloss, summary = sess.run([loss, merged], feed_dict={tf_x: x, tf_y: y, lr: learning_rate})
                 print("Iter " + str(steps) + ", Minibatch Loss= " + "{:.6f}".format(bloss))
                 summary_writer.add_summary(summary, steps)
 
