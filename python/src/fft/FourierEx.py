ec5cf5689be106d6481864ef203714a0f660566f
kakaba2009 <kakaba2009@gmail.com>
2017-07-15 18:29:22 +0900

    Add Tensor Flow

--- python/src/fft/FourierEx.py
+++ python/src/fft/FourierEx.py
@@ -3,20 +3,20 @@ import pylab as plt
 from numpy import fft
 import src.mylib.mlstm as mlstm
 
-ds = mlstm.loadFXData('JPY=X', '../db/forex.db', 4096)
+ds = mlstm.loadFXData('JPY=X', '../db/forex.db', 1024)
 x = ds[['Close']].values
-
+x = x.flatten()
 #t = np.arange(256)
 #x = np.sin(t)
 
 n = x.size
 spectrum = fft.fft(x)
-feq = fft.fftfreq(n)  # frequencies
+feq = fft.fftfreq(n)            # frequencies
 
 ampli = np.absolute(spectrum)   # amplitude
 phase = np.angle(spectrum)      # phase
 print(phase)
 
-plt.plot(feq, spectrum.real, feq, spectrum.imag)
+plt.plot(ampli)
 plt.legend()
 plt.show()
