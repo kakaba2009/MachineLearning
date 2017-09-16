7eb17a48567eb3a0263f55bd3eb3be5e58b07aa8
kakaba2009 <kakaba2009@gmail.com>
2017-09-16 17:12:39 +0900

    Pandas Yahoo API

--- python/saveFXData.py
+++ python/saveFXData.py
@@ -1,10 +1,11 @@
+import time
 import datetime
 import numpy as np
 import pandas_datareader.data as web
 from sqlalchemy import create_engine
 import src.mylib.mfile as mfile
 
-Symbols = mfile.loadSymbolList("src/db/forex.db")
+Symbols = mfile.loadSymbolList("src/db/source.db")
 
 Symbols = Symbols.values
 
@@ -12,10 +13,15 @@ print(Symbols)
 
 for Symbol in Symbols:
     print(Symbol, "\n")
-    start = datetime.datetime(1900, 1, 1)
-    end = datetime.datetime(2036, 12, 31)
+    start = datetime.datetime(1980, 1, 1)
+    end = datetime.datetime(2017, 12, 31)
 
-    df = web.get_data_yahoo(Symbol, start, end)
+    try:
+        time.sleep(1)
+        df = web.get_data_yahoo(Symbol, start, end)
+    except Exception as e:
+        print(Symbol, e)
+        continue
     
     #Covert from panel to data frame
     df = df.to_frame()
@@ -26,10 +32,9 @@ for Symbol in Symbols:
     
     print(df.values, "\n")
 
-    disk_engine = create_engine('sqlite:///src/db/forex1.db')
+    disk_engine = create_engine('sqlite:///src/db/forex.db')
 
-    df.to_sql("FX", disk_engine, if_exists='append');
-    
+    df.to_sql("FX", disk_engine, if_exists='append')
     #save csv files
     #filename = 'src/db/' + Symbol + '.csv' 
     #df.to_csv(filename, mode='a', columns=['Close'], header=False, index=False)
