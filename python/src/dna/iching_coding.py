439e34611f48ac00762cc947f632d84980d4a70f
kakaba2009 <kakaba2009@gmail.com>
2017-09-18 12:11:40 +0900

    Show 3 Graphs

--- python/src/dna/iching_coding.py
+++ python/src/dna/iching_coding.py
@@ -12,13 +12,14 @@ guai =int('111110', 2) #101010 I Ching 43
 qian =int('111111', 2) #101010 I Ching 01
 kun  =int('000000', 2) #101010 I Ching 02
 
-df = mfile.loadOneSymbol("JPY=X", "../db/forex.db")
 def dateToDouble(d):
     return (d.year * 10000.0 + d.month * 100.0 + d.day) / 10000.0
+
+dat = mfile.loadOneSymbol("JPY=X", "../db/forex.db")
 #df.index = df.index.map(dateToDouble)
-df = df[['Close']] #return data frame
-df = df.head(100)
-df = df.diff()
+dat = dat[['Close']] #return data frame
+dat = dat.head(200)
+df  = dat.diff()
 #df = df.dropna()
 fn = lambda x: (1.0 if x > 0.0 else 0.0)
 xx = df['Close'].apply(fn)
@@ -31,9 +32,11 @@ L3 = xx.shift(-3)
 L4 = xx.shift(-4)
 L5 = xx.shift(-5)
 yy = L0 * 32 + L1 * 16 + L2 * 8 + L3 * 4 + L4 * 2 + L5
-#zz = np.copy(yy)
-#zz[zz != stop] = np.nan
+zz = yy.copy()
+zz[zz != stop] = np.nan
 style.use('ggplot')
-plt.scatter(yy.index, yy)
+plt.plot(yy.index, yy)
+plt.plot(zz.index, zz, "bo")
+plt.plot(dat.index, dat['Close']/2.0)
 print(yy)
 plt.show()
