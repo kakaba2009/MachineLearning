1b011874217a0c09f5655d9801286d0f1f3ce4bb
kakaba2009 <kakaba2009@gmail.com>
2017-09-18 11:21:01 +0900

    Update IChing Coding

--- python/src/dna/iching_coding.py
+++ python/src/dna/iching_coding.py
@@ -15,13 +15,13 @@ kun  =int('000000', 2) #101010 I Ching 02
 df = mfile.loadOneSymbol("JPY=X", "../db/forex.db")
 def dateToDouble(d):
     return (d.year * 10000.0 + d.month * 100.0 + d.day) / 10000.0
-df.index = df.index.map(dateToDouble)
-df = df['Close']
-df = df.tail(100)
+#df.index = df.index.map(dateToDouble)
+df = df[['Close']] #return data frame
+df = df.head(100)
 df = df.diff()
-df = df.dropna()
+#df = df.dropna()
 fn = lambda x: (1.0 if x > 0.0 else 0.0)
-xx = df.apply(fn)
+xx = df['Close'].apply(fn)
 L0 = xx
 print(L0)
 L1 = xx.shift(-1)
@@ -34,6 +34,6 @@ yy = L0 * 32 + L1 * 16 + L2 * 8 + L3 * 4 + L4 * 2 + L5
 #zz = np.copy(yy)
 #zz[zz != stop] = np.nan
 style.use('ggplot')
-yy.plot()
-#print(yy)
+plt.scatter(yy.index, yy)
+print(yy)
 plt.show()
