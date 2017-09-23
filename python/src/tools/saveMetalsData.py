038013b982049fafa1e66fc14aeb0992a97b182c
kakaba2009 <kakaba2009@gmail.com>
2017-09-23 19:30:33 +0900

    Resample Daily Yearly

--- python/src/tools/saveMetalsData.py
+++ python/src/tools/saveMetalsData.py
@@ -6,17 +6,21 @@ from sqlalchemy import create_engine
 start = datetime.datetime(1975, 1, 1)
 end = datetime.datetime(2017, 12, 30)
 
-Symbols = ['CHRIS/CME_GC1']
-Symbols = ['CHRIS/CME_SI1']
+Symbols = ['CHRIS/CME_GC1', 'CHRIS/CME_SI1']
+
+SampleF = ['daily', 'weekly', 'monthly', 'quarterly', 'annual']
 
 for Symbol in Symbols:
-    print(Symbol, "\n")
-    df = quandl.get(Symbol)
+    for Sample in SampleF:
+        print(Symbol, "=>", Sample)
+        df = quandl.get(Symbol, collapse=Sample)
 
-    df.insert(0, "Symbol", Symbol, True)
+        df['Close'] = df['Last']
 
-    print(df, "\n")
+        df.insert(0, "Symbol", Symbol, True)
 
-    disk_engine = create_engine('sqlite:///../db/Metals.db')
+        print(df, "\n")
 
-    df.to_sql("Metal", disk_engine, if_exists='append');
+        disk_engine = create_engine('sqlite:///../db/Metals.db')
+
+        df.to_sql(Sample, disk_engine, if_exists='append');
